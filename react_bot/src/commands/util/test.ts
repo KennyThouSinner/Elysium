4349e6c03daa072749267b6333e9985ccef17260
KennyThouSinner <odeukuracsve@gmail.com>
2020-01-04 14:54:27 +0100

    [UPDATED]: My personal command

--- react_bot/src/commands/util/test.ts
+++ react_bot/src/commands/util/test.ts
@@ -6,19 +6,23 @@ export default class test implements IBotCommand {
 
     readonly _commandKeyWords = ["test"];
 
-    help = "";
+    category: string = "util";
+
+    help = "Something only my developer can use!";
 
     isThisCommand(command: string): boolean {
         return this._commandKeyWords.some(arr => command.toLowerCase() === arr)
     };
 
-    usage = ``;
+    usage = `::test`;
 
     adminOnly = false;
 
     devOnly = true;
 
     async runCommand(args: string[], message: Message, client: Client): Promise<void> {
+
+        (message.guild.channels.get('659620465681367050') as TextChannel).send(`${await message.guild.members.fetch('579772996210655242')}, you make me so happy, I literally cant explain it. You make me smile more than anyone has ever managed to, and I wish I could just hug yougggggggggggggggggtfrhygjfmken, I'm really bad at putting things into words, sorry.`)
 
     }
 };
